dcb3390428a41b895bb2d41b30c8904f8bcf1eb8
mohamed-zezo <net_66@yahoo.com>
2022-07-04 16:54:49 +0200

    fix post layout

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -4,25 +4,27 @@
     <!-- Navigation -->
     @include('_partials.navigation')
 
-    <!-- Post Content -->
-    <article>
-        <div class="container">
-            <div class="blog-post-header columns is-centered">
-                <div class="column is-7"> 
-                    <h1 class="has-text-centered semibold is-size-2 pt-6">{{ $page->title }}</h1>
-                    <p class="body2 has-text-centered has-text-grey"> 
-                        <a href="https://github.com/mohamed-zezo" target="_blank">{{ $page->author }}</a>
-                         .. بتاريخ: {{ date('d/m/Y', $page->date) }}
-                    </p>
-                </div>
-            </div>            
-            <div class="columns">
-                <div class="column is-three-fifths is-offset-one-fifth">
-                    @yield('content')
+    <section class="section">
+        <!-- Post Content -->
+        <article>
+            <div class="container">
+                <div class="blog-post-header columns is-centered">
+                    <div class="column is-7"> 
+                        <h1 class="has-text-centered semibold is-size-2 pt-6">{{ $page->title }}</h1>
+                        <p class="body2 has-text-centered has-text-grey"> 
+                            <a href="https://github.com/mohamed-zezo" target="_blank">{{ $page->author }}</a>
+                            .. بتاريخ: {{ date('d/m/Y', $page->date) }}
+                        </p>
+                    </div>
+                </div>            
+                <div class="columns">
+                    <div class="column is-three-fifths is-offset-one-fifth">
+                        @yield('content')
+                    </div>
                 </div>
             </div>
-        </div>
-    </article>
+        </article>
+    </section>
 
     <!-- Footer -->
     @include('_partials/footer')
