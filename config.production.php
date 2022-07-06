6b7e9be4f07b79318fbdec6fe88856de4fa515fb
mohamed-zezo <net_66@yahoo.com>
2022-07-06 18:05:06 +0200

    remove www

--- config.production.php
+++ config.production.php
@@ -2,7 +2,7 @@
 
 return [
     'production' => true,
-    'baseUrl' => 'https://www.onemorebug.tk',
+    'baseUrl' => 'https://onemorebug.tk',
     'title' => 'One more bug',
     'description' => 'موقع ومدونة محمد عبد العزيز | one more bug',
 ];
